438307f62aaae012c76b77654868b9de07786e2f
kaleopiasse <kaleopiasse@tokenlab.com.br>
2024-03-04 13:14:30 -0300

    feat: finish navbar desktop and mobile component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { BreakpointService } from '../../core/services/breakpoint.service';
+import { tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
+  isOpenMenuBar = false;
+  isMobile = false;
+
+  constructor(private readonly _breakpointService: BreakpointService) {}
+
+  ngOnInit(): void {
+    this._breakpointService.mediaBreakpoint$
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        tap(media => {
+          this.isMobile = (media === 'xs' || media === 'sm' || media === 'md');
+        })
+      )
+      .subscribe();
+  }
+
+}
